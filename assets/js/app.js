195f81e317060597c377f6b3bc04301dd6fe0cb6
lemunao <pedroandres1@gmail.com>
2022-06-30 22:09:52 -0400

    Fix in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,4 @@
-let boton = document.querySelector('#boton');
-let valores = [document.querySelector('cantidad'), document.querySelector('color')]
-console.log(valores[0], valores)
-boton.addEventListener('submit', () => {
-    let cantidad = document.querySelector('#cantidad');
-    console.log(cantidad);
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault()    //Captura de datos
+    document.getElementById("result").innerHTML += `<li>total de venta $: ${5990 * document.getElementById("cantidad").value} ${document.getElementById("cantidad").value} Cazuelas</li><li>Color: ${document.getElementById("color").value}</li>`;
 })
